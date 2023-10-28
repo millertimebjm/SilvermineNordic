6aa3b2d69fcd59b02ae223d07e2ea6119042c0c7
Brandon Miller <millertimebjm@hotmail.com>
2023-10-28 18:47:24 -0500

    add default threshold when in memory setting

--- ApiServer/SilvermineNordic.Repository/Services/EntityFrameworkThresholdService.cs
+++ ApiServer/SilvermineNordic.Repository/Services/EntityFrameworkThresholdService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SilvermineNordic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace SilvermineNordic.Repository.Services
 {
     public class EntityFrameworkThresholdService : IRepositoryThreshold
     {
+        private const string INMEMORY = "Microsoft.EntityFrameworkCore.InMemory";
         private readonly SilvermineNordicDbContext _dbContext;
         public EntityFrameworkThresholdService(SilvermineNordicDbContext dbContext)
         {
@@ -18,10 +20,14 @@ namespace SilvermineNordic.Repository.Services
             int? count = null,
             int? skip = null)
         {
+            if (_dbContext.Database.ProviderName == INMEMORY
+                && (await _dbContext.Thresholds.FirstOrDefaultAsync()) == null)
+            {
+                await SeedData();
+            }
+
             return await _dbContext
-                .Thresholds.AsQueryable()
-                .Skip(skip ?? 0)
-                .Take(count ?? int.MaxValue)
+                .Thresholds
                 .ToListAsync();
         }
 
@@ -36,6 +42,19 @@ namespace SilvermineNordic.Repository.Services
             await _dbContext.AddAsync(threshold);
             await _dbContext.SaveChangesAsync();
             return threshold;
+        }
+
+        private async Task SeedData()
+        {
+            await _dbContext.Thresholds.AddAsync(new Threshold()
+            {
+                Id = 0,
+                HumidityHighThreshold = 10m,
+                HumidityLowThreshold = 0m,
+                TemperatureInCelciusHighThreshold = 11m,
+                TemperatureInCelciusLowThreshold = 1m,
+            });
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
