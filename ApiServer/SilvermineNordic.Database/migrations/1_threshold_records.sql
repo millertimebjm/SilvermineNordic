30e795bacbb1ab8f48b312855e4489437e206e21
Brandon Miller <millertimebjm@hotmail.com>
2023-11-25 22:37:40 -0600

    Millertimebjm/main adminmvc zoneforecast (#70)
    * add zone forecast to Homepage
    
    * add some percent signs and update threshold sql

--- ApiServer/SilvermineNordic.Database/migrations/1_threshold_records.sql
+++ ApiServer/SilvermineNordic.Database/migrations/1_threshold_records.sql
@@ -1,8 +1,9 @@
 INSERT INTO Threshold
 (TemperatureInCelciusLowThreshold, TemperatureInCelciusHighThreshold, HumidityLowThreshold, HumidityHighThreshold)
 VALUES
-(-10, -7, .2, 1),
-(-6, -5, .2, .9),
-(-5, -4, .2, .7),
-(-4, -3, .2, .5),
-(-3, -2, .2, .3)
+(-100, -3, 20, 100),
+(-3, -2, 20, 90),
+(-2, -1, 20, 70),
+(-1, -0, 20, 50),
+(0, 1, 20, 40),
+(1, 2, 20, 30)
