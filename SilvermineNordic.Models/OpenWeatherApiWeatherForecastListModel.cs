a8fed250e5bc1a3501bcca1aacc14adf7742c2c5
Brandon Miller <millertimebjm@hotmail.com>
2025-06-11 11:38:25 -0500

    fix OpenWeatherApi model warnings

--- SilvermineNordic.Models/OpenWeatherApiWeatherForecastListModel.cs
+++ SilvermineNordic.Models/OpenWeatherApiWeatherForecastListModel.cs
@@ -27,9 +27,9 @@ namespace SilvermineNordic.Models;
     public class City
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public Coord Coord { get; set; }
-    public string Country { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public Coord Coord { get; set; } = new();
+    public string Country { get; set; } = string.Empty;
     public int Population { get; set; }
     public int Timezone { get; set; }
     public int Sunrise { get; set; }
@@ -59,16 +59,16 @@ public class OpenWeatherApiWeatherForecastList
             return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(Dt);
         }
     }
-    public Main Main { get; set; }
-    public List<Weather> Weather { get; set; }
-    public Clouds Clouds { get; set; }
-    public Wind Wind { get; set; }
+    public Main Main { get; set; } = new();
+    public List<Weather> Weather { get; set; } = new();
+    public Clouds Clouds { get; set; } = new();
+    public Wind Wind { get; set; } = new();
     public int Visibility { get; set; }
     public double Pop { get; set; }
     public Precipitation? Rain { get; set; }
     public Precipitation? Snow { get; set; }
-    public Sys Sys { get; set; }
-    public string DtTxt { get; set; }
+    public Sys Sys { get; set; } = new();
+    public string DtTxt { get; set; } = string.Empty;
 }
 
 public class Main
@@ -94,24 +94,24 @@ public class Precipitation
 
 public class OpenWeatherApiWeatherForecastRoot
 {
-    public string Cod { get; set; }
+    public string Cod { get; set; } = string.Empty;
     public int Message { get; set; }
     public int Cnt { get; set; }
-    public List<OpenWeatherApiWeatherForecastList> List { get; set; }
-    public City City { get; set; }
+    public List<OpenWeatherApiWeatherForecastList> List { get; set; } = new();
+    public City City { get; set; } = new();
 }
 
 public class Sys
 {
-    public string Pod { get; set; }
+    public string Pod { get; set; } = string.Empty;
 }
 
 public class Weather
 {
     public int Id { get; set; }
-    public string Main { get; set; }
-    public string Description { get; set; }
-    public string Icon { get; set; }
+    public string Main { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Icon { get; set; } = string.Empty;
 }
 
 public class Wind
