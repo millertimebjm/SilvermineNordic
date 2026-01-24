175418da567653457a516a9025e417037d713688
Brandon Miller <millertimebjm@hotmail.com>
2026-01-24 09:33:55 -0600

    add build artifact files

---
+++ SilvermineNordic.RefreshWeather/build_2_create_docker_artifacts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Delete docker container FORCED if it exists
+# Your image name
+IMAGE_NAME="silverminenordic-refreshweather"
+
+
+dotnet publish --os linux --arch x64 /t:PublishContainer -c Release
+docker run -d -p 9070:9070 --restart=always --name silverminenordic-refreshweather -e AppConfigConnectionString="$1" silverminenordic-refreshweather:latest
+rm -rf /tmp/Containers
+
+
+
+# Check if there are any dangling images
+if [ -n "$(docker images -f dangling=true -q)" ]; then
+    # Remove dangling images
+    docker rmi --force $(docker images -f "dangling=true" -q)
+else
+    echo "No dangling images found."
+fi
